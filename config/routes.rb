7a1bed109728ef7dd5a8d8e6cf1c60c57fddfd53
DungNguyen_HUST <vandungbknd@gmail.com>
2020-04-04 10:59:48 +0700

    remove header and footer cotroller by page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   delete  "logout"   => "sessions#destroy"
   get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :headers
   resources :users
-  root "header#index"
+  resources :pages
+  root "pages#index"
 end
