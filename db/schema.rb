f0ccb60a948b9c840198e9e6bb273bdb82e7e0e9
DungNguyen_HUST <vandungbknd@gmail.com>
2020-04-22 23:34:33 +0700

    add postressql

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_04_22_105345) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -31,14 +34,6 @@ ActiveRecord::Schema.define(version: 2020_04_22_105345) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "username"
-    t.text "comment"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "post_id"
   end
 
   create_table "companies", force: :cascade do |t|
@@ -56,8 +51,18 @@ ActiveRecord::Schema.define(version: 2020_04_22_105345) do
     t.datetime "time_end"
   end
 
-# Could not dump table "company_interviews" because of following StandardError
-#   Unknown type 'bool' for column 'offer'
+  create_table "company_interviews", force: :cascade do |t|
+    t.string "user_name"
+    t.string "position"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "company_id"
+    t.integer "difficultly"
+    t.integer "satisfied"
+    t.integer "process"
+    t.boolean "offer"
+  end
 
   create_table "company_reply_interviews", force: :cascade do |t|
     t.string "user_name"
@@ -89,14 +94,6 @@ ActiveRecord::Schema.define(version: 2020_04_22_105345) do
     t.text "pros"
     t.text "cons"
     t.text "review_title"
-  end
-
-  create_table "headers", force: :cascade do |t|
-    t.string "logoImage"
-    t.string "buttonName"
-    t.text "searchIcon"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "post_comments", force: :cascade do |t|
