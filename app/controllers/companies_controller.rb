91d1e0cfcbb96d28be899200148c42f4ea39df21
DungNguyen_HUST <vandungbknd@gmail.com>
2020-04-25 00:10:27 +0700

    add rating sys

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,11 @@
 class CompaniesController < ApplicationController
+    helper_method :rating_total_score
+    helper_method :rating_work_env_score
+    helper_method :rating_salary_score
+    helper_method :rating_ot_score
+    helper_method :rating_manager_score
+    helper_method :rating_career_score
+
     def index
         @companies = Company.all
         @company_reviews = CompanyReview.all
@@ -44,6 +51,36 @@ class CompaniesController < ApplicationController
         @company = Company.find params[:id]
         @company.destroy
         redirect_to company_path
+    end
+
+    def rating_total_score
+        rate_work_env = @company.company_reviews.sum('work_env_score') / @company.company_reviews.count
+        rate_salary = @company.company_reviews.sum('salary_score') / @company.company_reviews.count
+        rate_ot = @company.company_reviews.sum('ot_score') / @company.company_reviews.count
+        rate_manager = @company.company_reviews.sum('manager_score') / @company.company_reviews.count
+        rate_career = @company.company_reviews.sum('career_score') / @company.company_reviews.count
+
+        total_rate = (rate_work_env + rate_salary + rate_ot + rate_manager + rate_career).to_f/5
+    end
+
+    def rating_work_env_score
+        rate_work_env = @company.company_reviews.sum('work_env_score').to_f / @company.company_reviews.count
+    end
+
+    def rating_salary_score
+        rate_salary = @company.company_reviews.sum('salary_score').to_f / @company.company_reviews.count
+    end
+
+    def rating_ot_score
+        rate_ot = @company.company_reviews.sum('ot_score').to_f / @company.company_reviews.count
+    end
+
+    def rating_manager_score
+        rate_manager = @company.company_reviews.sum('manager_score').to_f / @company.company_reviews.count
+    end
+
+    def rating_career_score
+        rate_career = @company.company_reviews.sum('career_score').to_f / @company.company_reviews.count
     end
 
     private
