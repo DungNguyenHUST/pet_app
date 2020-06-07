d7113cdc2cc857fe853904b613d7f60475e1411b
DungNguyenHUST <vandungbknd@gmail.com>
2020-06-07 18:37:44 +0700

    add user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,9 @@ class User < ApplicationRecord
     # using encryt pass
     has_secure_password
     has_one_attached :avatar
+    has_one_attached :cover_letter_attach
 
     has_many :company_like_reviews, dependent: :destroy
     has_many :company_dislike_reviews, dependent: :destroy
-
-    validate :image_type
-
-    private
-
-    def image_type
-        if avatar.attached? == false
-            errors.add(:avatar, "is missing!")
-        end
-        if !avatar.content_type.in?(%('image/jpeg image/png'))
-            errors.add(:avatar, "needs to be a jpeg or png!")
-        end
-    end
+    
 end
