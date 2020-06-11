9b716f9e38e8912c1222f94142bcd2bd07f3323b
DungNguyenHUST <vandungbknd@gmail.com>
2020-06-11 23:35:12 +0700

    add wall pic

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ApplicationRecord
     has_many :company_reviews, dependent: :destroy
     has_many :company_interviews, dependent: :destroy
     has_one_attached :avatar
+    has_one_attached :wall_picture
     has_rich_text :content
     has_rich_text :policy
 
