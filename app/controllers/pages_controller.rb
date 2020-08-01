c74f90a2be77382174b1500ce023be3fc2c14fee
DungNguyenHUST <vandungbknd@gmail.com>
2020-08-01 17:52:04 +0700

    abc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
         @user = User.new
         @companies = Company.all
         @company_jobs = CompanyJob.all
+        @company_reviews = CompanyReview.all
     end
 end
