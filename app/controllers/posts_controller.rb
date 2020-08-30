a0bdd1f853b206762f57f4110e2a4a4f1d7ee8ae
DungNguyenHUST <vandungbknd@gmail.com>
2020-08-30 07:07:27 +0700

    working on August

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,7 @@ class PostsController < ApplicationController
     def show
         @post = Post.find params[:id]
         @post_comment = PostComment.new
+        @posts = Post.all
     end
 
     def edit
