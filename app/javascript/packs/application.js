fc20587827468cfb5f64b7426ffb26816af3bc79
DungNguyenHUST <vandungbknd@gmail.com>
2021-04-25 17:42:08 +0700

    fix yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import "bootstrap"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -27,5 +28,3 @@ require("@rails/actiontext")
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-import "bootstrap"
