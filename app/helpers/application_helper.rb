9629545ce6a8a91109c08939b634d3a9d33e91f3
DungNguyenHUST <vandungbknd@gmail.com>
2021-04-28 15:14:44 +0700

    fix date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
     def time_ago_in_words(from_time, include_seconds_or_options = {})
         distance_of_time_in_words(from_time, Time.now, include_seconds_or_options)
     end
+
+    def since_date(end_time)
+    	cal_date = (end_time - DateTime.now)/(24*60*60)
+    end
 	
 	def default_meta_tags
     {
