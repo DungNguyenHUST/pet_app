da71eae85954a0631af4033bfe867044d3da8349
DungNguyenHUST <vandungbknd@gmail.com>
2021-04-29 23:05:35 +0700

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "bootstrap"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -27,3 +26,5 @@ require("@rails/actiontext")
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+import "bootstrap"
