706f6b3d53005f2c80fdd068c6a5d23fa9c1796f
DungNguyenHUST <vandungbknd@gmail.com>
2021-05-20 15:56:34 +0700

    fix button

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :save_my_previous_url, only: [:new]
+  # before_action :save_my_previous_url, only: [:new]
 
   # GET /resource/sign_in
   # def new
@@ -10,32 +10,27 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  def create
-    if current_user.employer? && !current_user.approved?
-      flash[:danger] = "Tài khoản nhà tuyển dụng của bạn đang được xử lý, vui lòng đăng nhập lại sau 30 min..."
-      redirect_to pages_path
-    else
-      case session[:my_previous_url]
-      when new_user_session_path
-        redirect_to pages_path
-      when new_user_path
-        redirect_to pages_path
-      when users_path
-        redirect_to pages_path
-      else
-        redirect_to session[:my_previous_url]
-      end
-    end
-    return
-    # super
-  end
+  # def create
+  #   case session[:my_previous_url]
+  #   when new_user_session_path
+  #     redirect_to pages_path
+  #   when new_user_path
+  #     redirect_to pages_path
+  #   when users_path
+  #     redirect_to pages_path
+  #   else
+  #     redirect_to session[:my_previous_url]
+  #   end
+  #   return
+  #   super
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
@@ -46,7 +41,7 @@ class Users::SessionsController < Devise::SessionsController
   #   stored_location_for(resource_or_scope) || root_path
   # end
 
-  def save_my_previous_url
-    session[:my_previous_url] = URI(request.referer || '').path
-  end
+  # def save_my_previous_url
+  #   session[:my_previous_url] = URI(request.referer || '').path
+  # end
 end
