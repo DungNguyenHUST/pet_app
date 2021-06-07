4bd3d5b93762b5c36f6078ac7c2f6be23587b256
DungNguyenHUST <vandungbknd@gmail.com>
2021-06-07 22:00:10 +0700

    fix page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,5 @@
 class CompaniesController < ApplicationController
+    include ::CompaniesHelper
     before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
     def index
         @is_company_searched = false
@@ -7,18 +8,16 @@ class CompaniesController < ApplicationController
 			@company_searchs = Company.friendly.search(params[:search]).approved
 		end
         @companies_all = Company.all.approved
-        @companies_oder_name = Company.all.order('name DESC').approved.page(params[:page]).per(10)
-        @companies_oder_newest = Company.all.order('created_at DESC').approved.page(params[:page]).per(10)
+        @companies_oder_name = Company.all.order('name DESC').approved.page(params[:page]).per(20)
+        @companies_oder_newest = Company.all.order('created_at DESC').approved.page(params[:page]).per(20)
         @companies_most_recent = Company.all.order('created_at DESC').approved.reverse
         @companies_best = []
         count = 0
         @companies_all.each do |company|
-			if company.approved?
-				if cal_company_score(company).to_f > count
-					@companies_best.push(company)
-					count = cal_company_score(company).to_f
-				end
-			end
+            if cal_rating_review_total_score(company).to_f > count
+                @companies_best.push(company)
+                count = cal_rating_review_total_score(company).to_f
+            end
         end
         @company_reviews = CompanyReview.all
         @company_interviews = CompanyInterview.all
@@ -119,19 +118,5 @@ class CompaniesController < ApplicationController
     # define param for each company
     def company_param
         params.require(:company).permit(:name, :location, :address, :country, :website, :phone, :time_establish, :working_time, :working_date, :size, :field_operetion, :avatar, :wall_picture, :search, :overview, :policy, :values, :company_type, {:benefit => []})
-    end
-
-    def cal_company_score(company)
-        if(company.company_reviews.count > 0)
-            rate_work_env = company.company_reviews.sum('work_env_score').to_f / company.company_reviews.count
-            rate_salary = company.company_reviews.sum('salary_score').to_f / company.company_reviews.count
-            rate_ot = company.company_reviews.sum('ot_score').to_f / company.company_reviews.count
-            rate_manager = company.company_reviews.sum('manager_score').to_f / company.company_reviews.count
-            rate_career = company.company_reviews.sum('career_score').to_f / company.company_reviews.count
-
-            total_rate = (rate_work_env + rate_salary + rate_ot + rate_manager + rate_career)/5
-        else
-            total_rate = 0
-        end
     end
 end
