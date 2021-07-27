b4c8f597a6b08173b5bff94956c0c90de3934cd1
DungNguyenHUST <vandungbknd@gmail.com>
2021-07-27 15:13:17 +0700

    add scraper

--- app/workers/scrap_worker.rb
+++ app/workers/scrap_worker.rb
@@ -6,187 +6,198 @@ class ScrapWorker
     end
 
 	def get_data_itviec(url, company_name)
-        # Root link
-		response = HTTParty.get(url.to_s)
-        doc = Nokogiri::HTML(response.body)
+		# get company id by name
+		@company = Company.friendly.search(company_name).approved.first
+		if(@company.present?)
+			company_id = @company.id
+        
+			# Root link
+			response = HTTParty.get(url.to_s)
+			doc = Nokogiri::HTML(response.body)
 
-        # Get job link in to map
-        processing_block = doc.css("h4.title")
-        links = processing_block.css("a").map { |link| link['href']}
-        links.map { |h| h.to_s.prepend("https://itviec.com/")}			
-		
-		# Start to get data
-		processing_datas = []
-		links.each do |link|
-			response = HTTParty.get(link.to_s)
-        	doc = Nokogiri::HTML(response.body)
-
-			title = doc.css("h1.job-details__title").first.text.strip
+			# Get job link in to map
+			processing_block = doc.css("h4.title")
+			links = processing_block.css("a").map { |link| link['href']}
+			links.map { |h| h.to_s.prepend("https://itviec.com/")}			
 			
-			# get detail
-			start_detail = doc.at_css("div.job-details__divider")
-			end_detail = doc.at_css("div.jd-page__employer-overview")
-			detail = ''
-			next_step = 0
-			loop do
-				break if next_step == 8 # stop before end_detail
-				start_detail = start_detail.next_element
-				detail << start_detail.to_s
-				next_step += 1
+			# Start to get data
+			processing_datas = []
+			links.each do |link|
+				response = HTTParty.get(link.to_s)
+				doc = Nokogiri::HTML(response.body)
+
+				title = doc.css("h1.job-details__title").first.text.strip
+				
+				# get detail job
+				start_detail = doc.at_css("div.job-details__divider")
+				end_detail = doc.at_css("div.jd-page__employer-overview")
+				detail = ''
+				next_step = 0
+				loop do
+					break if next_step == 8 # stop before end_detail
+					if !start_detail.next_element.nil?
+						start_detail = start_detail.next_element
+						detail << start_detail.to_s
+					end
+					next_step += 1
+				end
+
+				location = "Hà Nội"
+				salary = "Thương lượng"
+				quantity = 1
+				category = "IT"
+				language = "Tùy chọn"
+				level = "Nhân viên"
+				dudate = Time.now
+				end_date = Time.now + 30.days
+				job_type = "Full Time"
+				urgent = false
+				apply_another_site_flag = true
+				apply_site = link.to_s
+				address = "Hà Nội"
+				approved = true
+				user_id = 1
+
+				data_temp = job_params.new(title,
+										detail,
+										location, 
+										salary, 
+										quantity, 
+										category,
+										language, 
+										level,
+										dudate,
+										end_date,
+										job_type, 
+										urgent, 
+										apply_another_site_flag, 
+										apply_site, 
+										address,
+										user_id,
+										approved,
+										company_id)
+
+				processing_datas.push(data_temp)
 			end
-			location = "Hà Nội"
-			salary = "Thương lượng"
-			quantity = 1
-			category = "IT"
-			language = "Tùy chọn"
-			level = "Nhân viên"
-			dudate = Time.now
-			end_date = Time.now + 30.days
-			job_type = "Full Time"
-			urgent = false
-			apply_another_site_flag = true
-			apply_site = link.to_s
-			address = "Hà Nội"
-			approved = true
-			user_id = 1
 
-			# get company id by path
-			if company_name == "FPT"
-				company_id = 5 #FPT
-			elsif company_name == "TECHCOMBANK"
-				company_id = 25
-			elsif company_name == "LG"
-				company_id = 5
-			else
-				company_id = 1
-			end
-
-			data_temp = job_params.new(title,
-									detail,
-									location, 
-									salary, 
-									quantity, 
-									category,
-									language, 
-									level,
-									dudate,
-									end_date,
-									job_type, 
-									urgent, 
-									apply_another_site_flag, 
-									apply_site, 
-									address,
-									user_id,
-									approved,
-									company_id)
-
-			processing_datas.push(data_temp)
+			return processing_datas
+		else
+			return nil
 		end
-
-		return processing_datas
 	end
 
 	def get_data_careerbuilder(url, company_name)
-        # Root link
-		response = HTTParty.get(url.to_s)
-        doc = Nokogiri::HTML(response.body)
+		# get company id by name
+		@company = Company.friendly.search(company_name).approved.first
+		if(@company.present?)
+			company_id = @company.id
+			# Root link
+			response = HTTParty.get(url.to_s)
+			doc = Nokogiri::HTML(response.body)
 
-        # Get job link in to map
-		processing_block = doc.css("div.figcaption div.title")
-		links = processing_block.css("a").map { |link| link['href']}
-		
-		# Start to get data
-		processing_datas = []
-		links.each do |link|
-			response = HTTParty.get(link.to_s)
-        	doc = Nokogiri::HTML(response.body)
-
-			title = doc.css("h1.title").first.text.strip
+			# Get job link in to map
+			processing_block = doc.css("div.figcaption div.title")
+			links = processing_block.css("a").map { |link| link['href']}
 			
-			# get detail
-			start_detail = doc.at_css("div.detail-row")
-			end_detail = doc.at_css("div.share-this-job")
-			detail = ''
-			next_step = 0
-			loop do
-				break if next_step == 3 # stop before end_detail
-				detail << start_detail.to_s
-				start_detail = start_detail.next_element
-				next_step += 1
+			# Start to get data
+			processing_datas = []
+			links.each do |link|
+				response = HTTParty.get(link.to_s)
+				doc = Nokogiri::HTML(response.body)
+
+				title = doc.css("h1.title").first.text.strip
+				
+				# get detail job
+				start_detail = doc.at_css("div.detail-row")
+				end_detail = doc.at_css("div.share-this-job")
+				detail = ''
+				next_step = 0
+				loop do
+					break if next_step == 3 # stop before end_detail
+					if !start_detail.next_element.nil?
+						start_detail = start_detail.next_element
+						detail << start_detail.to_s
+					end
+					next_step += 1
+				end
+
+				location = "Hà Nội"
+				salary = "Thương lượng"
+				quantity = 1
+				category = "IT"
+				language = "Tùy chọn"
+				level = "Nhân viên"
+				dudate = Time.now
+				end_date = Time.now + 30.days
+				job_type = "Full Time"
+				urgent = false
+				apply_another_site_flag = true
+				apply_site = link.to_s
+				address = "Hà Nội"
+				approved = true
+				user_id = 1
+
+				# get company id by name
+				@company = Company.friendly.search(company_name).approved.first
+				if(@company.present?)
+					company_id = @company.id
+				else
+					company_id = Company.first.id
+				end
+
+				data_temp = job_params.new(title,
+										detail,
+										location, 
+										salary, 
+										quantity, 
+										category,
+										language, 
+										level,
+										dudate,
+										end_date,
+										job_type, 
+										urgent, 
+										apply_another_site_flag, 
+										apply_site, 
+										address,
+										user_id,
+										approved,
+										company_id)
+
+				processing_datas.push(data_temp)
 			end
-			location = "Hà Nội"
-			salary = "Thương lượng"
-			quantity = 1
-			category = "IT"
-			language = "Tùy chọn"
-			level = "Nhân viên"
-			dudate = Time.now
-			end_date = Time.now + 30.days
-			job_type = "Full Time"
-			urgent = false
-			apply_another_site_flag = true
-			apply_site = link.to_s
-			address = "Hà Nội"
-			approved = true
-			user_id = 1
 
-			# get company id by path
-			if company_name == "FPT"
-				company_id = 5 #FPT
-			elsif company_name == "TECHCOMBANK"
-				company_id = 25
-			elsif company_name == "LG"
-				company_id = 5
-			else
-				company_id = 1
-			end
-
-			data_temp = job_params.new(title,
-									detail,
-									location, 
-									salary, 
-									quantity, 
-									category,
-									language, 
-									level,
-									dudate,
-									end_date,
-									job_type, 
-									urgent, 
-									apply_another_site_flag, 
-									apply_site, 
-									address,
-									user_id,
-									approved,
-									company_id)
-
-			processing_datas.push(data_temp)
+			return processing_datas
+		else
+			return nil
 		end
-
-		return processing_datas
 	end
 
 	def processing_job(job_datas)
-		job_datas.each do |job_data|
-			@company = Company.find_by_id(job_data.company_id)
-			@company_job = @company.company_jobs.create!(:title => job_data.title,
-														:detail => job_data.detail,
-														:location => job_data.location,
-														:salary => job_data.salary,
-														:quantity => job_data.quantity,
-														:category => job_data.category,
-														:language => job_data.language,
-														:level => job_data.level,
-														:dudate => job_data.dudate,
-														:end_date => job_data.end_date,
-														:job_type => job_data.job_type,
-														:urgent => job_data.urgent,
-														:apply_another_site_flag => job_data.apply_another_site_flag,
-														:apply_site => job_data.apply_site,
-														:address => job_data.address,
-														:user_id => job_data.user_id,
-														:approved => job_data.approved)
-			@company_job.save!
+		if job_datas.nil?
+			return
+		else
+			job_datas.each do |job_data|
+				@company = Company.find_by_id(job_data.company_id)
+				@company_job = @company.company_jobs.create!(:title => job_data.title,
+															:detail => job_data.detail,
+															:location => job_data.location,
+															:salary => job_data.salary,
+															:quantity => job_data.quantity,
+															:category => job_data.category,
+															:language => job_data.language,
+															:level => job_data.level,
+															:dudate => job_data.dudate,
+															:end_date => job_data.end_date,
+															:job_type => job_data.job_type,
+															:urgent => job_data.urgent,
+															:apply_another_site_flag => job_data.apply_another_site_flag,
+															:apply_site => job_data.apply_site,
+															:address => job_data.address,
+															:user_id => job_data.user_id,
+															:approved => job_data.approved)
+				@company_job.save!
+			end
 		end
 	end
 
