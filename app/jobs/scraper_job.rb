7ed5bc6a2052953cd65dde06defdaf884f7820d6
DungNguyenHUST <vandungbknd@gmail.com>
2021-08-06 22:35:15 +0700

    validate url

--- app/jobs/scraper_job.rb
+++ app/jobs/scraper_job.rb
@@ -6,6 +6,20 @@ include ApplicationHelper
         processing_data(scrap_job)
     end
 
+    def check_exist_url(url)
+        url = URI.parse(url) rescue false
+        if url
+            req = Net::HTTP.new(url.host, url.port)
+            res = req.request_head(url.path)
+        end
+        
+        if res.present?
+            return true
+        else
+            return false
+        end
+    end
+
     def get_data_itviec(scrap_job)
         # get company id by name
         if scrap_job.company_name.present?
@@ -29,67 +43,68 @@ include ApplicationHelper
 
             if links.present?
                 links.each do |link|
-                    response = HTTParty.get(link.to_s)
-                    doc = Nokogiri::HTML(response.body)
+                    if check_exist_url(link)
+                        response = HTTParty.get(link.to_s)
+                        doc = Nokogiri::HTML(response.body)
 
-                    title = doc.css("h1.job-details__title").first.text.strip
-                    
-                    # get detail job
-                    start_detail = doc.at_css("div.job-details__divider")
-                    end_detail = doc.at_css("div.jd-page__employer-overview")
-                    detail = ''
-                    next_step = 0
-                    loop do
-                        break if next_step == 8 # stop before end_detail
-                        if !start_detail.next_element.nil?
-                            start_detail = start_detail.next_element
-                            detail << start_detail.to_s
+                        title = doc.css("h1.job-details__title").first.text.strip
+                        
+                        # get detail job
+                        start_detail = doc.at_css("div.job-details__divider")
+                        end_detail = doc.at_css("div.jd-page__employer-overview")
+                        detail = ''
+                        next_step = 0
+                        loop do
+                            break if next_step == 8 # stop before end_detail
+                            if !start_detail.next_element.nil?
+                                start_detail = start_detail.next_element
+                                detail << start_detail.to_s
+                            end
+                            next_step += 1
                         end
-                        next_step += 1
-                    end
-                    
-                    if scrap_job.location.present?
-                        location = scrap_job.location.to_s
-                    else
-                        location = "Hà Nội"
-                    end
-                    salary = "Thương lượng"
-                    quantity = 1
-                    category = "IT"
-                    language = "Tùy chọn"
-                    level = "Nhân viên"
-                    dudate = Time.now
-                    end_date = Time.now + 30.days
-                    job_type = "Full Time"
-                    urgent = false
-                    apply_another_site_flag = true
-                    apply_site = link.to_s
-                    address = @company.address
-                    approved = true
-                    user_id = 1
+                        
+                        if scrap_job.location.present?
+                            location = scrap_job.location.to_s
+                        else
+                            location = "Hà Nội"
+                        end
+                        salary = "Thương lượng"
+                        quantity = 1
+                        category = "IT"
+                        language = "Tùy chọn"
+                        level = "Nhân viên"
+                        dudate = Time.now
+                        end_date = Time.now + 30.days
+                        job_type = "Full Time"
+                        urgent = false
+                        apply_another_site_flag = true
+                        apply_site = link.to_s
+                        address = @company.address
+                        approved = true
+                        user_id = 1
 
-                    data_temp = job_params.new(title,
-                                detail,
-                                location, 
-                                salary, 
-                                quantity, 
-                                category,
-                                language, 
-                                level,
-                                dudate,
-                                end_date,
-                                job_type, 
-                                urgent, 
-                                apply_another_site_flag, 
-                                apply_site, 
-                                address,
-                                user_id,
-                                approved,
-                                @company.id)
+                        data_temp = job_params.new(title,
+                                    detail,
+                                    location, 
+                                    salary, 
+                                    quantity, 
+                                    category,
+                                    language, 
+                                    level,
+                                    dudate,
+                                    end_date,
+                                    job_type, 
+                                    urgent, 
+                                    apply_another_site_flag, 
+                                    apply_site, 
+                                    address,
+                                    user_id,
+                                    approved,
+                                    @company.id)
 
-                    processing_datas.push(data_temp)
+                        processing_datas.push(data_temp)
+                    end
                 end
-
                 return processing_datas
             else
                 return nil
@@ -120,67 +135,68 @@ include ApplicationHelper
             processing_datas = []
             if links.present?
                 links.each do |link|
-                    response = HTTParty.get(link.to_s)
-                    doc = Nokogiri::HTML(response.body)
+                    if check_exist_url(link)
+                        response = HTTParty.get(link.to_s)
+                        doc = Nokogiri::HTML(response.body)
 
-                    title = doc.css("h1.title").first.text.strip
-                    
-                    # get detail job
-                    start_detail = doc.at_css("div.detail-row")
-                    end_detail = doc.at_css("div.share-this-job")
-                    detail = ''
-                    next_step = 0
-                    loop do
-                        break if next_step == 3 # stop before end_detail
-                        if !start_detail.next_element.nil?
-                            start_detail = start_detail.next_element
-                            detail << start_detail.to_s
+                        title = doc.css("h1.title").first.text.strip
+                        
+                        # get detail job
+                        start_detail = doc.at_css("div.detail-row")
+                        end_detail = doc.at_css("div.share-this-job")
+                        detail = ''
+                        next_step = 0
+                        loop do
+                            break if next_step == 3 # stop before end_detail
+                            if !start_detail.next_element.nil?
+                                start_detail = start_detail.next_element
+                                detail << start_detail.to_s
+                            end
+                            next_step += 1
                         end
-                        next_step += 1
+
+                        if scrap_job.location.present?
+                            location = scrap_job.location.to_s
+                        else
+                            location = "Hà Nội"
+                        end
+                        salary = "Thương lượng"
+                        quantity = 1
+                        category = "IT"
+                        language = "Tùy chọn"
+                        level = "Nhân viên"
+                        dudate = Time.now
+                        end_date = Time.now + 30.days
+                        job_type = "Full Time"
+                        urgent = false
+                        apply_another_site_flag = true
+                        apply_site = link.to_s
+                        address = @company.address
+                        approved = true
+                        user_id = 1
+
+                        data_temp = job_params.new(title,
+                                    detail,
+                                    location, 
+                                    salary, 
+                                    quantity, 
+                                    category,
+                                    language, 
+                                    level,
+                                    dudate,
+                                    end_date,
+                                    job_type, 
+                                    urgent, 
+                                    apply_another_site_flag, 
+                                    apply_site, 
+                                    address,
+                                    user_id,
+                                    approved,
+                                    @company.id)
+
+                        processing_datas.push(data_temp)
                     end
-
-                    if scrap_job.location.present?
-                        location = scrap_job.location.to_s
-                    else
-                        location = "Hà Nội"
-                    end
-                    salary = "Thương lượng"
-                    quantity = 1
-                    category = "IT"
-                    language = "Tùy chọn"
-                    level = "Nhân viên"
-                    dudate = Time.now
-                    end_date = Time.now + 30.days
-                    job_type = "Full Time"
-                    urgent = false
-                    apply_another_site_flag = true
-                    apply_site = link.to_s
-                    address = @company.address
-                    approved = true
-                    user_id = 1
-
-                    data_temp = job_params.new(title,
-                                detail,
-                                location, 
-                                salary, 
-                                quantity, 
-                                category,
-                                language, 
-                                level,
-                                dudate,
-                                end_date,
-                                job_type, 
-                                urgent, 
-                                apply_another_site_flag, 
-                                apply_site, 
-                                address,
-                                user_id,
-                                approved,
-                                @company.id)
-
-                    processing_datas.push(data_temp)
                 end
-
                 return processing_datas
             else
                 return nil
@@ -251,11 +267,16 @@ include ApplicationHelper
 
     def processing_data(scrap_job)
         puts "Start scrap data..."
-        if split_domain_name(scrap_job.url) == "itviec.com"
-            processing_job(get_data_itviec(scrap_job))
-        end
-        if split_domain_name(scrap_job.url) == "careerbuilder.vn"
-            processing_job(get_data_careerbuilder(scrap_job))
+        if check_exist_url(scrap_job.url)
+            if split_domain_name(scrap_job.url) == "itviec.com"
+                processing_job(get_data_itviec(scrap_job))
+            end
+        
+            if split_domain_name(scrap_job.url) == "careerbuilder.vn"
+                processing_job(get_data_careerbuilder(scrap_job))
+            end
+        else
+            puts "Wrong url"
         end
         puts "End scrap data!!!"
     end
