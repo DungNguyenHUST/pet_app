dfa8cd9c1cbcca9c0e9af618dac3d4d419d2e4bf
DungNguyenHUST <vandungbknd@gmail.com>
2021-11-25 16:39:46 +0700

    fix I18n

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,2 +1,17 @@
 module ProblemsHelper
+    def convert_diff(diff)
+        str = ""
+        if diff
+            if diff.to_i == 1 || diff.to_s == t(:easy)
+                str = t(:easy)
+            elsif diff.to_i == 2 || diff.to_s == t(:average)
+                str = t(:average)
+            elsif diff.to_i == 3 || diff.to_s == t(:hard)
+                str = t(:hard)
+            else
+                str = t(:easy)
+            end
+        end
+        return str
+    end
 end
