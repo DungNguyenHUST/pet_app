876a093a943540c721148e9f13b5082cb1e6b8d1
DungNguyenHUST <vandungbknd@gmail.com>
2022-04-12 11:25:16 +0700

    add schedule for production

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,27 +23,27 @@ set :path, "#{path}"
 set :output, "#{path}/log/cronjob.log"
 
 #Sets the environment to run during development mode (Set to production by default)
-set :environment, 'development'
+# set :environment, 'development'
 
-if @environment == 'development'
-    set :bundle_command, "/home/dungnguyen/.rbenv/shims/bundle exec"
+# if @environment == 'development'
+#     set :bundle_command, "/home/dungnguyen/.rbenv/shims/bundle exec"
 
-    every :day, at: '0:01' do
-        rake "pull_job_tasks:job_puller"
-    end
+#     every :day, at: '0:01' do
+#         rake "pull_job_tasks:job_puller"
+#     end
 
-    every :day, at: '12:01' do
-        rake "pull_job_tasks:job_puller"
-    end
+#     every :day, at: '12:01' do
+#         rake "pull_job_tasks:job_puller"
+#     end
     
-    every :day, at: '6:06' do
-        command "sh /home/dungnguyen/pet_app/auto_upload.sh"
-    end
+#     every :day, at: '6:06' do
+#         command "sh /home/dungnguyen/pet_app/auto_upload.sh"
+#     end
 
-    every :day, at: '18:06' do
-        command "sh /home/dungnguyen/pet_app/auto_upload.sh"
-    end
-else
+#     every :day, at: '18:06' do
+#         command "sh /home/dungnguyen/pet_app/auto_upload.sh"
+#     end
+# else
     set :bundle_command, "/home/deploy/.rbenv/shims/bundle exec"
 
     every :day, at: '7:07' do
@@ -53,6 +53,6 @@ else
     every :day, at: '19:07' do
         rake "push_job_tasks:job_pusher"
     end
-end
+# end
 
 # Learn more: http://github.com/javan/whenever
